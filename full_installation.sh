ef8f5c4aa08cb8e3b9fee52620592284ad41af18
mflova <mflovaa@gmail.com>
2022-04-30 12:17:31 +0200

    feat: first installation version

---
+++ full_installation.sh
@@ -0,0 +1,33 @@
+ansible-playbook --ask-become developer/install_developer_tools.yaml
+source ~/.bashrc
+
+if [ $? != 0 ]; then
+    echo "Developer tools not correctly installed."
+    exit 1
+fi
+
+ansible-playbook --ask-become mflova-i3-setup/install_i3.yaml
+source ~/.bashrc
+
+if [ $? != 0 ]; then
+    echo "i3wm not correctly isntalled."
+    exit 1
+fi
+
+ansible-playbook --ask-become mflova-vim-config/script/ubuntu-setup.yaml
+source ~/.bashrc
+
+if [ $? != 0 ]; then
+    echo "Ubuntu setup for neovim not correctly installed."
+    exit 1
+fi
+
+ansible-playbook --ask-become mflova-vim-config/script/full-vim-installation.yaml
+source ~/.bashrc
+
+if [ $? != 0 ]; then
+    echo "Full neovim installation not correctly done."
+    exit 1
+fi
+
+echo "Full installation succeeded."
